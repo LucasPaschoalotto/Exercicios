a126ccf8dacdf2808bdd451786c6b5e1526d73c8
LucasPaschoalotto <lucas.paschoaloto@hotmail.com>
2022-07-04 14:05:08 -0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const listaNomes = require("./components/listaNomes.json");
-var consoleLista = listaNomes.forEach(item => console.log(item));
+var consoleLista = listaNomes.forEach(pessoa => console.log(pessoa));
 
 var listaFiltrada = listaNomes.map(pessoa => console.log(pessoa.nome));
